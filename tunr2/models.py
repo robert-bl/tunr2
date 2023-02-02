811f6921bf8afe29143c611915dc7cfe1dcac1ae
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-02-02 14:25:46 -0500

    gitignore photos

--- tunr2/models.py
+++ tunr2/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos')
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=100, default='no song title')
+    album = models.CharField(max_length=100, default='no album title')
+    preview_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
